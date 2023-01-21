52af891baa1259dc42905e710ca8074ff0aee085
Tatiana Dovzhich <dovzhich.tatyana@gmail.com>
2023-01-21 22:50:37 +0200

    [Lesson 16_Homework 16] Add waiters:
    - Add implicitly wait on driver level (conftest.py)
    - Add URLs for CreatPost and Post Pages (base.py)

--- conftest.py
+++ conftest.py
@@ -2,6 +2,9 @@ import pytest
 from selenium import webdriver
 
 from constants.base import BaseConstants
+from pages.create_post_page import CreatePostPage
+from pages.hello_page import HelloPage
+from pages.post_page import PostPage
 from pages.start_page import StartPage
 
 
@@ -19,3 +22,27 @@ def start_page(driver):
     driver.get(BaseConstants.URL)
     driver.implicity_wait(1)
     return StartPage(driver)
+
+
+@pytest.fixture()
+def hello_page(driver):
+    """Creates hello page object"""
+    driver.get(BaseConstants.URL)
+    driver.implicity_wait(1)
+    return HelloPage(driver)
+
+
+@pytest.fixture()
+def create_post_page(driver):
+    """Creates create_post page object"""
+    driver.get(BaseConstants.URL_CREATE_POST)
+    driver.implicity_wait(1)
+    return CreatePostPage(driver)
+
+
+@pytest.fixture()
+def post_page(driver):
+    """Creates post page object"""
+    driver.get(BaseConstants.URL_POST)
+    driver.implicity_wait(1)
+    return PostPage
--- constants/base.py
+++ constants/base.py
@@ -1,3 +1,6 @@
 class BaseConstants:
     URL = "https://qa-complexapp.onrender.com"
+    URL_CREATE_POST = "https://qa-complexapp.onrender.com/create-post"
+    URL_POST = "https://qa-complexapp.onrender.com/post/63cc49d43125d7003407e9e6"
+
     DRIVER_PATH = "../drivers/chromedriver"
