bf4169b0de3e3568bcc4ba40efd76c0437e66dcb
Tatiana Dovzhich <dovzhich.tatyana@gmail.com>
2023-01-23 20:05:40 +0200

    [Lesson 16_Homework 16] Fix#01: Delete unnecessary wrong drivers at conftest.py

--- conftest.py
+++ conftest.py
@@ -2,9 +2,6 @@ import pytest
 from selenium import webdriver
 
 from constants.base import BaseConstants
-from pages.create_post_page import CreatePostPage
-from pages.hello_page import HelloPage
-from pages.post_page import PostPage
 from pages.start_page import StartPage
 
 
@@ -24,25 +21,4 @@ def start_page(driver):
     return StartPage(driver)
 
 
-@pytest.fixture()
-def hello_page(driver):
-    """Creates hello page object"""
-    driver.get(BaseConstants.URL)
-    driver.implicity_wait(1)
-    return HelloPage(driver)
 
-
-@pytest.fixture()
-def create_post_page(driver):
-    """Creates create_post page object"""
-    driver.get(BaseConstants.URL_CREATE_POST)
-    driver.implicity_wait(1)
-    return CreatePostPage(driver)
-
-
-@pytest.fixture()
-def post_page(driver):
-    """Creates post page object"""
-    driver.get(BaseConstants.URL_POST)
-    driver.implicity_wait(1)
-    return PostPage
