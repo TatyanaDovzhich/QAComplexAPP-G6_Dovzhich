47ef4b4accc24ef5d40e23586db5e8b19d1b5b32
Tatiana Dovzhich <dovzhich.tatyana@gmail.com>
2023-01-23 21:15:38 +0200

    [Lesson 16_Homework 16] Fix#02: Delete unnecessary wrong URLs at constants/base.py

--- constants/base.py
+++ constants/base.py
@@ -1,6 +1,4 @@
 class BaseConstants:
     URL = "https://qa-complexapp.onrender.com"
-    URL_CREATE_POST = "https://qa-complexapp.onrender.com/create-post"
-    URL_POST = "https://qa-complexapp.onrender.com/post/63cc49d43125d7003407e9e6"
 
     DRIVER_PATH = "../drivers/chromedriver"
